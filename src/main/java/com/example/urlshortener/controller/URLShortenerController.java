843c27c8fc43c4df2e6089db920bc95485fe429c
Trung Ma <trung.ma@iths.se>
2024-05-08 12:13:40 +0200

    Created Dockerfile

--- src/main/java/com/example/urlshortener/controller/URLShortenerController.java
+++ src/main/java/com/example/urlshortener/controller/URLShortenerController.java
@@ -1,4 +1,9 @@
 package com.example.urlshortener.controller;
 
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class URLShortenerController {
+
 }
