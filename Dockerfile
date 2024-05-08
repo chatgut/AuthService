05460b7471bc5bda7b15cb8d4c7f397d4b723db0
Trung Ma <trung.ma@iths.se>
2024-05-08 13:20:05 +0200

    Added methods in RestController

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM eclipse-temurin:21-jre-alpine
 
 WORKDIR /app
 
-COPY target/URLShortener.jar app.jar
+COPY target/URLShortener-0.0.1-SNAPSHOT.jar app.jar
 
 EXPOSE 8080
 
--- src/main/java/com/example/urlshortener/controller/URLShortenerController.java
+++ src/main/java/com/example/urlshortener/controller/URLShortenerController.java
@@ -1,9 +1,36 @@
 package com.example.urlshortener.controller;
 
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 @RestController
 public class URLShortenerController {
 
+    private final Map<String, String> urlMap = new HashMap<>();
+
+    @PostMapping("/shorten")
+    public String shortenUrl(@RequestBody String longUrl) {
+        String shortUrl = generateShortUrl();
+        urlMap.put(shortUrl, longUrl);
+        return shortUrl;
+    }
+
+    private static String generateShortUrl() {
+        UUID uuid = UUID.randomUUID();
+        String shortUrl = uuid.toString().substring(0, 7);
+        return shortUrl;
+    }
+
+    @GetMapping("/{shortUrl}")
+    public String redirect(@PathVariable String shortUrl) {
+        String longUrl = urlMap.get(shortUrl);
+        if (longUrl != null) {
+            return "redirect:" + longUrl;
+        } else {
+            return "Shortened URL not found";
+        }
+    }
 }
