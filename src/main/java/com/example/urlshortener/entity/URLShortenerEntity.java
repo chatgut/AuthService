2715d8ae88c1b8009111bb358535420a1972f85e
Trung Ma <trung.ma@iths.se>
2024-05-09 12:04:44 +0200

    updated entityclass

--- src/main/java/com/example/urlshortener/entity/URLShortenerEntity.java
+++ src/main/java/com/example/urlshortener/entity/URLShortenerEntity.java
@@ -1,13 +1,34 @@
 package com.example.urlshortener.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class URLShortenerEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private String longURL;
     private String shortURL;
 
+    public URLShortenerEntity() {
+    }
+
     public URLShortenerEntity(String longURL, String shortURL) {
         this.longURL = longURL;
         this.shortURL = shortURL;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getLongURL() {
